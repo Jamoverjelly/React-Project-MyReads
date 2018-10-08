05b8273bfcfafd3bdbf9a247b468948044e38e86
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-10-08 17:16:16 -0400

    Search Page is now aware of all text entered into search bar which is updated as query state

--- src/searchPage.js
+++ src/searchPage.js
@@ -6,6 +6,13 @@ class SearchPage extends Component {
     searchedBooksList: []
   }
 
+  udpateQuery = (query) => {
+    // modify query state based on argument passed into updateQuery as 'query'
+    this.setState({
+      query: query
+    })
+  }
+
   render() {
     return (
       <div className="search-books">
@@ -13,7 +20,14 @@ class SearchPage extends Component {
           <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
           
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              // set value of input field to current query state
+              value={this.state.query}
+              // any text entered in input is captured as event and passed into udpateQuery, updating the query state
+              onChange={(event) => this.udpateQuery(event.target.value)}
+            />
           </div>
         </div>
 
